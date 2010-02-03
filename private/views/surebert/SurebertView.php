118d1cb231abfa4bd453e78efd63019a2ddde1e0
visco <visco@bcca41d9-2e01-4a91-b83d-1fc52eeb338b>
2010-02-03 01:27:29 +0000

    updated surebert view to offer new basic surebert view which serves most used surebert files grouped together

--- private/views/surebert/SurebertView.php
+++ private/views/surebert/SurebertView.php
@@ -2,44 +2,43 @@
 /**
  * Creates concatenated javascript files for the surebert toolkit from the arguments it is fed
  * @author visco
- * @version 1.11 05/20/2008 02/04/2009
  *
  */
 class SurebertView extends sb_View{
-	
+
 	/**
 	 * Determines if caching is used
 	 * @var boolean
 	 */
 	public $cache_enable = false;
-	
+
 	/**
 	 * The base files included with all custom or surebert views, the default is sb for sb.js
 	 * @var string
 	 */
 	protected $default_files = Array();
-	
+
 	/**
 	 * Removes comments from javascript
 	 * @author visco
 	 */
 	protected function filter_output($output){
-		
+
 		if(!isset($this->request->get['sb_comments'])){
             return preg_replace("~/\*\*.*?\*/~s", "", $output);
         } else {
             return $output;
         }
 	}
-	
+
 	/**
 	 * Concatenates the javascript/surebert files and turns them into cache if caching is enable
 	 * @param $files
 	 */
 	protected function concat_files($files = Array()){
-		
+
 		$binary = preg_match("~\.(swf|gif|png)$~", $files[0], $match);
-		
+
 		if($binary){
 			if($match[1] == 'swf'){
 				header("Content-type: application/x-shockwave-flash");
@@ -49,67 +48,67 @@ class SurebertView extends sb_View{
 		} else {
 			$this->add_javascript_headers();
 		}
-		
+
 		if($this->cache_enable){
 			$cache = isset(App::$cache) ? App::$cache : new sb_Cache_FileSystem();
 			$key = '/toolkit/'.md5(join(",", $this->request->args));
-			
+
 			$data = $cache->fetch($key);
 			if($data){
 				echo $data;
 				return true;
 			}
 		}
-		
+
 		$surebert = $this->default_files;
-		
+
 		foreach($files as $file){
 			if($binary){
 				$surebert[] = $file;
 			} else {
-				
+
 				$surebert[] = str_replace('.', '/', $file).'.js';
 			}
 		}
-		
+
 		ob_start();
-		
+
 		foreach($surebert as $file){
-			
+
 			$path = SUREBERT_TOOLKIT_PATH.'/'.$file;
-			
-			if($file == 'sb.js'){	
+
+			if($file == 'sb.js'){
 				echo "var sbBase = '/surebert/load';\n";
 			}
-			
+
 			if(is_file($path)){
-				
+
 				readfile($path);
 			} else {
 				echo"\nthrow('ERROR: Surebert module \"".basename($file)."\" could not be located by /surebert/load ');";
 			}
 		}
-		
+
 		$js = ob_get_clean();
 		echo $js;
-		
+
 		if($this->cache_enable){
 			$cache->store($key, $js);
 		}
-		
+
 	}
-	
+
 	/**
 	 * Adds javascript headers to the file and adds cache control if this is the main
 	 * view being displayed.  If it is included in another view do not do this
 	 * as you don't want HTML being served as javascript
 	 */
 	protected function add_javascript_headers(){
-		
+
 		if(!$this->included){
 			header("Content-type: application/x-javascript");
 		}
-		
+
 	}
 }
 ?>
