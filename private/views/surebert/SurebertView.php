d7b1f9ee0c63ec150dd16512d50800f18d2fbbac
visco <visco@bcca41d9-2e01-4a91-b83d-1fc52eeb338b>
2010-02-03 02:20:46 +0000

    no longer loads js1_5 from client for IE, you need to load it yourself or use new surebert framework /surebert/load or /surebert/basic view

--- private/views/surebert/SurebertView.php
+++ private/views/surebert/SurebertView.php
@@ -2,6 +2,7 @@
 /**
  * Creates concatenated javascript files for the surebert toolkit from the arguments it is fed
  * @author visco
+ * @version 1.11 05/20/2008 02/04/2009
  *
  */
 class SurebertView extends sb_View{
@@ -19,39 +20,66 @@ class SurebertView extends sb_View{
 	protected $default_files = Array();
 
 	/**
+	 * The toolkit tag to use for serving the js from surebert
+	 * @var string
+	 */
+	public $toolkit_root = '/var/www/sbf/toolkit/';
+
+	/**
+	 * The version of surebert being served
+	 * @var string
+	 */
+	public $version;
+
+	/**
 	 * Removes comments from javascript
 	 * @author visco
 	 */
 	protected function filter_output($output){
 
-		if(!isset($this->request->get['sb_comments'])){
-            return preg_replace("~/\*\*.*?\*/~s", "", $output);
-        } else {
-            return $output;
-        }
+		return preg_replace("~/\*\*.*?\*/~s", "", $output);
 	}
 
 	/**
 	 * Concatenates the javascript/surebert files and turns them into cache if caching is enable
 	 * @param $files
 	 */
-	protected function concat_files($files = Array()){
+	protected function concat_files($files = Array(), $version=''){
 
-		$binary = preg_match("~\.(swf|gif|png)$~", $files[0], $match);
+		if($files[0] == 'sb' && strstr(Gateway::$agent, 'MSIE')){
+
+			array_unshift($files, 'js1_5');
+		}
+
+		$version = !empty($version) ? $version : SUREBERT_TOOLKIT_PATH;
+
+		$tag = $this->toolkit_root.'/tags/'.$version;
+		if(is_numeric($version) && is_dir($tag)){
+			$root = $this->toolkit_root.'tags/'.$version;
+		} else {
+			$root = $this->toolkit_root.'trunk';
+		}
+
+		$this->version = basename($root);
+
+
+		$binary = preg_match("~\.(swf|gif)$~", $files[0], $match);
 
 		if($binary){
 			if($match[1] == 'swf'){
 				header("Content-type: application/x-shockwave-flash");
-			} else if($match[1] == 'gif' || $match[1] == 'png'){
-				header("Content-type: image/".$match[1]);
+			} else if($match[1] == 'gif'){
+				header("Content-type: image/gif");
 			}
 		} else {
 			$this->add_javascript_headers();
+			echo '//v '.$this->version.' - '.date('m/d/Y H:i:s')."\n";
+
 		}
 
 		if($this->cache_enable){
 			$cache = isset(App::$cache) ? App::$cache : new sb_Cache_FileSystem();
-			$key = '/toolkit/'.md5(join(",", $this->request->args));
+			$key = '/toolkit/'.md5(implode(",", $files).$version);
 
 			$data = $cache->fetch($key);
 			if($data){
@@ -75,11 +103,7 @@ class SurebertView extends sb_View{
 
 		foreach($surebert as $file){
 
-			$path = SUREBERT_TOOLKIT_PATH.'/'.$file;
-
-			if($file == 'sb.js'){
-				echo "var sbBase = '/surebert/load';\n";
-			}
+			$path = $root.'/'.$file;
 
 			if(is_file($path)){
 
