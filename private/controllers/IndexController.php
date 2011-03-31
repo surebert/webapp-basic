2dbd159019fc5456afb5a478941fd332b7569bc5
visco <visco@bcca41d9-2e01-4a91-b83d-1fc52eeb338b>
2011-03-31 04:13:42 +0000

    the new controller branch is being merged into trunk after the previous trunk was saved in tag 1.6

---
+++ private/controllers/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * An example that serves the index.view
+ * @author visco
+ *
+ */
+class IndexController extends sb_Controller_HTML5{
+
+	/**
+	 * This sets the page title for a .view
+	 * @var string
+	 */
+	public $title = 'My App Homepage';
+
+	/**
+	 * This runs before the page is rendered and allows you to check args etc.
+	 * If you return false, e.g. if certain login criteria are not met etc, the page is not rendered for the user
+	 */
+	public function on_before_render(){
+
+		$this->styles[] = 'app.css';
+
+		//do something if post data is set
+		//if(isset($this->request->post['test'])){//do somthing}
+	}
+
+	public function not_found($template){
+		parent::not_found();
+	}
+}
+?>
